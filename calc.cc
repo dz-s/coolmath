28e2402f4c04291dd092e676d3d1e4737efbc089
teriyakigod2 <shulhin_dv@st.by>
2018-02-14 23:33:55 +0300

    Update calc.cc

--- calc.cc
+++ calc.cc
@@ -51,6 +51,21 @@ NAN_METHOD(sub)
     info.GetReturnValue().Set(truncate((double)(a - b), precision));
 }
 
+NAN_METHOD(sum)
+{
+    if (!info[0]->IsNumber() || !info[1]->IsNumber())
+    {
+        Nan::ThrowTypeError("arguments must be a number!");
+        return;
+    }
+
+    double a = (double)info[0]->NumberValue();
+    double b = (double)info[1]->NumberValue();
+    unsigned int precision = (unsigned int)info[2]->NumberValue();
+
+    info.GetReturnValue().Set(truncate((double)(a + b), precision));
+}
+
 NAN_MODULE_INIT(Initialize)
 {
     NAN_EXPORT(target, div);
